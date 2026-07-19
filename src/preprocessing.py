7c5a46c648f6b1ce3e7df17150c14670cfbe60ab
Tiaaan12 <christiandevera232@gmail.com>
2026-07-19 23:26:27 +0800

    import

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -37,4 +37,11 @@ def load_process(file_path):
     df = df.drop(columns=['dob'], axis=1)
 
     X = df.drop(columns=['is_fraud'], axis=1)
+
+    from sklearn.ensemble import IsolationForest
+
+
+    model = IsolationForest(random_state=42, n_estimators=283, contamination=0.008946057697978307, max_samples=0.44241943884813695, bootstrap=False)
+    model.fit(X)
+
     return df
