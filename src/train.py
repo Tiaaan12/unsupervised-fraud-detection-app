69d637a32950bb18f04d5b8b6afcd53a7a5205c1
Tiaaan12 <christiandevera232@gmail.com>
2026-07-19 23:30:36 +0800

    model function

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import IsolationForest
 
-
-model = IsolationForest(random_state=42, n_estimators=283, contamination=0.008946057697978307, max_samples=0.44241943884813695, bootstrap=False)
+def model_train(df):
+    model = IsolationForest(random_state=42, n_estimators=283, contamination=0.008946057697978307, max_samples=0.44241943884813695, bootstrap=False)
+    
