ab359e1ecc6a77c95dd0a4deb631ed772076fb5f
Tiaaan12 <christiandevera232@gmail.com>
2026-07-19 23:28:18 +0800

    preparing model

---
+++ src/train.py
@@ -0,0 +1,4 @@
+from sklearn.ensemble import IsolationForest
+
+
+model = IsolationForest(random_state=42, n_estimators=283, contamination=0.008946057697978307, max_samples=0.44241943884813695, bootstrap=False)
